ead39c72fbef78654f9756f04aa0fa5622e8bcc3
Revital Sur <eres@il.ibm.com>
2021-01-21 08:53:18 +0200

    Refactor code

--- manager/secretprovider/secretapis.go
+++ manager/secretprovider/secretapis.go
@@ -6,7 +6,6 @@ package secretprovider
 import (
 	"context"
 	b64 "encoding/base64"
-	"encoding/json"
 	"errors"
 	"fmt"
 	"net/http"
@@ -19,31 +18,77 @@ import (
 	"k8s.io/apimachinery/pkg/util/rand"
 )
 
+const tokenReviewNamespace = "default"
+const serviceAccountPrefix = "system:serviceaccount:"
+
 /*func GetSecretFromBackend(w http.ResponseWriter, r *http.Request) {
-	provider.Log.Info(fmt.Sprintf("In GetSecretFromBackend"))
+	log := provider.Log
+	ctx := context.Background()
+
+	log.Info(fmt.Sprintf("REVIT   GetSecretFromBackend Failed: %s", module))
 	datasetCreds := `{"access_key": "x", "secret_key": "x"}"`
 	render.JSON(w, r, datasetCreds) // Return the M4DApplication as json
 }*/
 
-func GetSecretFromBackend(w http.ResponseWriter, r *http.Request) {
+// Check if the sevice account token is authorized to access the secret.
+func isAuthorized(token string) error {
 	log := provider.Log
 	ctx := context.Background()
-	decoder := json.NewDecoder(r.Body)
-	decoder.DisallowUnknownFields()
+	random := rand.String(5)
+	randomTokenReviewName := "m4d-token-review-" + random
 
-	secret := r.URL.Query().Get("secret")
-	if secret == "" {
-		err := errors.New("Url Param 'secret' is missing")
-		suberr := render.Render(w, r, ErrInvalidRequest(err))
-		if suberr != nil {
-			log.Error(err, fmt.Sprintf("GetSecretFromBackend Failed: %s", err.Error()))
-		}
-		return
+	log.Info(fmt.Sprintf(fmt.Sprintf("In isAuthorized created tokenReview %s\n", randomTokenReviewName)))
+
+	authClient := provider.Clientset
+	genTokenReview := &auth.TokenReview{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "TokenReview",
+			APIVersion: "authentication.k8s.io/v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{Name: randomTokenReviewName, Namespace: tokenReviewNamespace},
+		Spec:       auth.TokenReviewSpec{Token: token},
+	}
+	trev, err := authClient.AuthenticationV1().TokenReviews().Create(ctx, genTokenReview, metav1.CreateOptions{})
+
+	if err != nil {
+		return err
+	}
+	// Check the status of the TokenReviews
+	if !trev.Status.Authenticated {
+		err := errors.New("Authorization Token is not Authenticated by TokenReview")
+		return err
 	}
 
-	tokenBase64Encoded := r.Header.Get("Authorization")
+	module := trev.Status.User.Username
+	// Check that the service account name is from the expected namespace
+	// FIXME: Fix namespace
+	if !strings.HasPrefix(module, serviceAccountPrefix+"m4d-system") {
+		return err
+	}
+	return nil
+}
+
+func isValidRequest(secret string, tokenBase64Encoded string) error {
+	if secret == "" {
+		err := errors.New("Url Param 'secret' is missing")
+		return err
+	}
+
 	if tokenBase64Encoded == "" {
 		err := errors.New("Authorization Token is missing from header")
+		return err
+	}
+	return nil
+}
+
+func GetSecretFromBackend(w http.ResponseWriter, r *http.Request) {
+	log := provider.Log
+
+	secret := r.URL.Query().Get("secret")
+	tokenBase64Encoded := r.Header.Get("Authorization")
+
+	err := isValidRequest(secret, tokenBase64Encoded)
+	if err != nil {
 		suberr := render.Render(w, r, ErrInvalidRequest(err))
 		if suberr != nil {
 			log.Error(err, fmt.Sprintf("GetSecretFromBackend Failed: %s", err.Error()))
@@ -52,43 +97,8 @@ func GetSecretFromBackend(w http.ResponseWriter, r *http.Request) {
 	}
 	token, _ := b64.URLEncoding.DecodeString(tokenBase64Encoded)
 
-	random := rand.String(5)
-	randomTokenReviewName := "m4d-token-review-" + random
-
-	log.Info(fmt.Sprintf(fmt.Sprintf("In GetSecretFromBackend created tokenReview %s\n", randomTokenReviewName)))
-
-	authClient := provider.Clientset
-	genTokenReview := &auth.TokenReview{
-		TypeMeta: metav1.TypeMeta{
-			Kind:       "TokenReview",
-			APIVersion: "authentication.k8s.io/v1",
-		},
-		ObjectMeta: metav1.ObjectMeta{Name: randomTokenReviewName, Namespace: "default"},
-		Spec:       auth.TokenReviewSpec{Token: string(token)},
-	}
-	trev, err := authClient.AuthenticationV1().TokenReviews().Create(ctx, genTokenReview, metav1.CreateOptions{})
-
+	err = isAuthorized(string(token))
 	if err != nil {
-		suberr := render.Render(w, r, ErrInvalidRequest(err))
-		if suberr != nil {
-			log.Error(err, fmt.Sprintf("GetSecretFromBackend Failed: %s", err.Error()))
-		}
-		return
-	}
-	// Check the status of the TokenReviews
-	if !trev.Status.Authenticated {
-		err := errors.New("Authorization Token is not Authenticated by TokenReview")
-		suberr := render.Render(w, r, ErrForbiddenRequest(err))
-		if suberr != nil {
-			log.Error(err, fmt.Sprintf("GetSecretFromBackend Failed: %s", err.Error()))
-		}
-		return
-	}
-
-	module := trev.Status.User.Username
-	log.Info(fmt.Sprintf("REVIT   GetSecretFromBackend Failed: %s", module))
-	if !strings.HasPrefix(module, "system:serviceaccount:"+"m4d-system") {
-		err := errors.New("Module is not authorized to access secret")
 		suberr := render.Render(w, r, ErrForbiddenRequest(err))
 		if suberr != nil {
 			log.Error(err, fmt.Sprintf("GetSecretFromBackend Failed: %s", err.Error()))
